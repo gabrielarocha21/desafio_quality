af5e34a1ab9fe5cfb758448954820915341568ab
afaeoliveira <rafael.aoliveira@mercadolivre.com>
2022-01-06 16:11:56 -0300

    teste rooService

---
+++ src/test/java/br/com/meli/bootcamp/desafio_quality/unit/service/RoomServiceTest.java
@@ -0,0 +1,23 @@
+package br.com.meli.bootcamp.desafio_quality.unit.service;
+
+
+import br.com.meli.bootcamp.desafio_quality.entities.RoomEntity;
+
+import br.com.meli.bootcamp.desafio_quality.service.RoomService;
+
+import org.junit.jupiter.api.Test;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class RoomServiceTest {
+
+    private RoomService roomService = new RoomService();
+
+    @Test
+    public void testArea() {
+        RoomEntity roomEntity = new RoomEntity("name", 3.0, 2.0);
+        Double area = this.roomService.getArea(roomEntity);
+        assertEquals(6.0, area);
+    }
+}
